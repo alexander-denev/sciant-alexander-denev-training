957fb0d8a5387a7f249fbbdb53f7320eb68ceb66
fugzel <aleksandar.d.denev@gmail.com>
2023-07-14 19:51:10 +0300

    wip, requires testing

--- JS Task/nodejs/components/routes/user/fetchUserdata.js
+++ JS Task/nodejs/components/routes/user/fetchUserdata.js
@@ -2,8 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
+  const userId = req.authObject.userId;
+  const limit = req.body.limit || 100;
+  const offset = req.body.offset || 0;
+  
   try {
-    const queryResult = await req.pool.query('SELECT userdata FROM users WHERE id = $1', [req.authObject.userId]);
+    const queryResult = await req.pool.query('SELECT userdata FROM users WHERE id = $1 OFFSET $2 LIMIT $3', [userId, offset, limit]);
     res.status(200).json({ userData: queryResult.rows[0].userdata });
   } catch (error) {
     res.status(500).json({ error: error });
